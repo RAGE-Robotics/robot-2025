7bb35674e0d3df989d07b0b5b1b691eb6334a533
Caleb Heydon <caleb.heydon@gmail.com>
2025-03-15 08:38:46 -0400

    Enable elevator limit switch

--- src/main/cpp/systems/Elevator.cpp
+++ src/main/cpp/systems/Elevator.cpp
@@ -44,9 +44,9 @@ Elevator::Elevator() : m_homeSwitch{Constants::kElevatorHomeSensorId} {
 }
 
 void Elevator::Update(Robot::Mode mode, double t) {
-  /*if (!m_homeSwitch.Get()) {
+  if (!m_homeSwitch.Get()) {
     m_mainMotor.SetPosition(units::turn_t{0});
-    }*/
+  }
 
   if (mode == Robot::Mode::kAuto || mode == Robot::Mode::kTeleop) {
     m_mainMotor.SetNeutralMode(signals::NeutralModeValue::Coast);
