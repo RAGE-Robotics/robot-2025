783958fe4745dddf9d1a9bbdbf34e2589da34d16
Caleb Heydon <caleb.heydon@gmail.com>
2025-03-19 20:53:54 -0400

    Don't sort tags

--- src/main/cpp/systems/Cameras.cpp
+++ src/main/cpp/systems/Cameras.cpp
@@ -10,13 +10,6 @@
 void Cameras::Update(Robot::Mode mode, double t) {
   // Front camera
   auto results = m_frontCamera.GetAllUnreadResults();
-
-  // See
-  // https://stackoverflow.com/questions/1380463/how-do-i-sort-a-vector-of-custom-objects
-  std::sort(results.begin(), results.end(), [](auto left, auto right) {
-    return left.GetTimestamp() < right.GetTimestamp();
-  });
-
   if (results.size() > 0) {
     auto pose = m_frontPoseEstimator.Update(results[0]);
 
@@ -28,11 +21,6 @@ void Cameras::Update(Robot::Mode mode, double t) {
 
   // Back camera
   results = m_backCamera.GetAllUnreadResults();
-
-  std::sort(results.begin(), results.end(), [](auto left, auto right) {
-    return left.GetTimestamp() < right.GetTimestamp();
-  });
-
   if (results.size() > 0) {
     auto pose = m_backPoseEstimator.Update(results[0]);
 
